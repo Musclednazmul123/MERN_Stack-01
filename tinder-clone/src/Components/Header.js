4e637daf4b2f0f9e0c51f633825d1a1c2f74555c
Nazmul Hawlader <97741145+Musclednazmul123@users.noreply.github.com>
2022-02-26 03:46:38 -0800

    02

--- tinder-clone/src/Components/Header.js
+++ tinder-clone/src/Components/Header.js
@@ -1,11 +1,18 @@
 import React from 'react';
 import "../Style/Header.css";
 import PersonIcon from '@mui/icons-material/Person';
+import IconButton  from '@mui/material/IconButton';
+import Tooltip  from '@mui/material/Tooltip';
 
 function Header() {
   return <div className='header'>
-        <PersonIcon />
-      </div>;
+    <Tooltip title="Profile">
+      <IconButton>
+        <PersonIcon fontSize='large' className='header__icon' />
+      </IconButton>
+    </Tooltip>
+    
+  </div>;
 
 }
 
