6a1e4f6163e2a9ead86fe67bc97f7a2dc22ec255
Nazmul Hawlader <97741145+Musclednazmul123@users.noreply.github.com>
2022-02-26 04:28:12 -0800

    Header update

--- tinder-clone/src/Components/Header.js
+++ tinder-clone/src/Components/Header.js
@@ -3,12 +3,29 @@ import "../Style/Header.css";
 import PersonIcon from '@mui/icons-material/Person';
 import IconButton  from '@mui/material/IconButton';
 import Tooltip  from '@mui/material/Tooltip';
+import ForumIcon from '@mui/icons-material/Forum';
 
 function Header() {
   return <div className='header'>
     <Tooltip title="Profile">
       <IconButton>
         <PersonIcon fontSize='large' className='header__icon' />
+      </IconButton>
+    </Tooltip>
+
+    <Tooltip title="Logo">
+      <IconButton>
+        <img 
+        className='header__logo'
+        src='https://logos-world.net/wp-content/uploads/2020/09/Tinder-Emblem.png'
+        alt='Tinder-logo'
+        />
+      </IconButton>
+
+    </Tooltip>
+    <Tooltip title="Profile">
+      <IconButton>
+        <ForumIcon fontSize='large' className='header__icon' />
       </IconButton>
     </Tooltip>
     
