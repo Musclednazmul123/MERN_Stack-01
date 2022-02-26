77272b2c56c635942f1f32cbe78b30d06c9e691d
Nazmul Hawlader <97741145+Musclednazmul123@users.noreply.github.com>
2022-02-26 03:31:01 -0800

    001

--- tinder-clone/src/App.js
+++ tinder-clone/src/App.js
@@ -1,13 +1,13 @@
 // import logo from './logo.svg';
 import React from 'react';
 import './App.css';
+import Header from './Components/Header';
 
 function App() {
     return ( 
         // BEM class naming convention
         <div className = "app" >
-            <h2 > hello world mmm</h2> 
-            {/* Headr */}
+            <Header />
             {/* TinderCards */}
             {/* SwipeButtons */}
         </div>
---
+++ tinder-clone/src/Components/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import "../Style/Header.css";
+import PersonIcon from '@mui/icons-material/Person';
+
+function Header() {
+  return <div className='header'>
+        <PersonIcon />
+      </div>;
+
+}
+
+export default Header;
